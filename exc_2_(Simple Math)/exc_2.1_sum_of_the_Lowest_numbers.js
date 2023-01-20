4a9ab6d6cd7d237f386de65c6755628c5e7de652
T1To48 <tofek.98@gmail.com>
2023-01-20 17:16:01 +0200

    #

--- exc_2_(Simple Math)/exc_2.1_sum_of_the_Lowest_numbers.js
+++ exc_2_(Simple Math)/exc_2.1_sum_of_the_Lowest_numbers.js
@@ -5,17 +5,11 @@
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 function sumLowst2(numsArr){
-   let m1n=Math.min(...numsArr);
-   
    if (numsArr.length>=4){
-     let minIndx=numsArr.indexOf(m1n);
-     numsArr.splice(minIndx);
-      let secondm1n = Math.min(...numsArr);
-      let sumOFLowest2=(m1n + secondm1n); 
-      return sumOFLowest2; 
+    numsArr.sort((a,b)=>{return a-b});
    }
    else {console.log('the minimum is 4 diffrent integers');}
-   
+   return numsArr[0]+numsArr[1];
  }
  let arrr=[10, 343445353, 3453445, 3453545353453];
  console.log(sumLowst2(arrr));
